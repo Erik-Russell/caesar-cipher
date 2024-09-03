9d7817ee302bdc044e7ce2a69791f5f31a0dcc26
Erik Russell <erik.z.russell@gmail.com>
2024-09-03 07:59:05 -0700

    Made GemFile and ran through RuboCop

--- cipher.rb
+++ cipher.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift_factor)
   # takes a string and shift_factor and outputs
   # modified string:
   # > caesar_cipher("What a string!", 5)
   # => "Bmfy f xywnsl!"
-  
+
   ########################
   #     ASCII CHART
   #   65 -  90  =>  A - Z
@@ -13,20 +15,20 @@ def caesar_cipher(string, shift_factor)
   uppercase_range = (65..90)
   lowercase_range = (97..122)
 
-  array_ords_original = string.chars.map { |char| char.ord }
+  array_ords_original = string.chars.map(&:ord)
 
   array_ords_shift = array_ords_original.map do |number|
     if uppercase_range.include?(number)
       number += shift_factor
       if number > 90
-        number -= 26
+        number - 26
       else
         number
       end
     elsif lowercase_range.include?(number)
       number += shift_factor
       if number > 122
-        number -= 26
+        number - 26
       else
         number
       end
@@ -34,10 +36,7 @@ def caesar_cipher(string, shift_factor)
       number
     end
   end
-  encrypted_string = array_ords_shift.map{ |num| num.chr }.join
-
-  return encrypted_string
-
+  array_ords_shift.map(&:chr).join
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
