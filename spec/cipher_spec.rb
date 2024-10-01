c023930d4b95b793a0f2064cc62a0ba6b091f29f
Erik Russell <erik.z.russell@gmail.com>
2024-10-01 10:47:12 -0700

    added two tests

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -2,4 +2,16 @@
 
 require_relative '../cipher'
 
-describe caesar_cipher
+describe '#caesar_cipher' do
+  # subject(caesar_cipher(string, shift))
+  it 'when given a good string and small positive shift' do
+    string = 'What a string!'
+    shift = 5
+    output = caesar_cipher(string, shift)
+    expect(output).to eq('Bmfy f xywnsl!')
+  end
+
+  it 'handles wrap around of chars' do
+    expect(caesar_cipher('xyz', 2)).to eq('zab')
+  end
+end
